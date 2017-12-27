7818ec8fd4389b74f27c3850cee81b0fee6881dd
Florian Rüberg <florian.rueberg@gmail.com>
2017-12-27 15:21:13 +0100

    Add scss and cleanup

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,23 +1,20 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
-
 import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
 import { HelloWorldModel } from './main-view-model';
 import { TNSPlayer } from 'nativescript-audio';
 import * as geolocation from "nativescript-geolocation";
 import { Accuracy } from "ui/enums";
+import { TimePicker } from "ui/time-picker";
 
-// Event handler for Page "navigatingTo" event attached in main-page.xml
+export function onPickerLoaded(args: EventData) {
+    let timePicker = <TimePicker>args.object;
+
+    timePicker.hour = 9;
+    timePicker.minute = 25;
+}
+
 export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
+
     let page = <Page>args.object;
 
     var _player = new TNSPlayer();
